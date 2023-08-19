1e17e63555776a1e7ec97a07a49c036565e84044
Ian Lyttle <ijlyttle@me.com>
2023-08-19 16:59:17 -0500

    add examples for functions

---
+++ R/functions-02-design-finished.R
@@ -0,0 +1,56 @@
+library("tidyverse")
+
+# Tidy evaluation
+
+# use {{embracing}} to set the x aesthetic
+histogram <- function(df, var, binwidth = NULL) {
+  df |>
+    ggplot(aes(x = {{var}})) +
+    geom_histogram(binwidth = binwidth)
+}
+
+# try your new function out:
+histogram(starwars, height)
+histogram(mtcars, mpg)
+
+# try specifying `theme_minimal()`
+histogram(mtcars, mpg) +
+  theme_minimal()
+
+# try specifying `fill = "steelblue"`
+# - need to modify function
+histogram <- function(df, var, ..., binwidth = NULL) {
+  df |>
+    ggplot(aes(x = {{var}})) +
+    geom_histogram(binwidth = binwidth, ...)
+}
+
+histogram(starwars, height, binwidth = 5, fill = "steelblue")
+
+## Using embracing for labels
+
+# Include the variable-name and binwidth in the title
+histogram <- function(df, var, ..., binwidth = NULL) {
+  df |>
+    ggplot(aes(x = {{ var }})) +
+    geom_histogram(binwidth = binwidth, ...) +
+    labs(
+      title = rlang::englue(
+        "Histogram of {{var}} using binwidth {binwidth %||% 'default'}"
+      )
+    )
+}
+
+histogram(starwars, height, binwidth = 5, fill = "steelblue")
+histogram(starwars, height, fill = "steelblue")
+
+## Using embracing to reorder data
+
+sorted_bars <- function(df, var) {
+  df |>
+    mutate({{ var }} := {{ var }} |> fct_infreq() |> fct_rev()) |>
+    ggplot(aes(y = {{ var }})) +
+    geom_bar()
+}
+
+diamonds |> sorted_bars(clarity)
